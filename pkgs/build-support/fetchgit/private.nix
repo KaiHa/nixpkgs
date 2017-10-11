378400a7d906c7de541803f620bcd7bfa28f4c49
Kai Harries <kai.harries@gmail.com>
2017-10-11 18:51:58 +0200

    fetchgitPrivate: Copy known_hosts file into the builder

--- pkgs/build-support/fetchgit/private.nix
+++ pkgs/build-support/fetchgit/private.nix
@@ -1,19 +1,15 @@
-{ fetchgit, writeScript, openssh, stdenv }: args: derivation ((fetchgit args).drvAttrs // {
+{ fetchgit, writeScript, openssh, stdenv, socat }: args: derivation ((fetchgit args).drvAttrs // rec {
+  passAsFile = [ "known_hosts" ];
+  known_hosts = if (builtins.tryEval <ssh-known-hosts>).success
+    then builtins.readFile <ssh-known-hosts>
+    else builtins.readFile ~/.ssh/known_hosts;
   SSH_AUTH_SOCK = if (builtins.tryEval <ssh-auth-sock>).success
     then builtins.toString <ssh-auth-sock>
-    else null;
+    else builtins.getEnv "SSH_AUTH_SOCK";
   GIT_SSH = writeScript "fetchgit-ssh" ''
     #! ${stdenv.shell}
-    exec -a ssh ${openssh}/bin/ssh -F ${let
-      sshConfigFile = if (builtins.tryEval <ssh-config-file>).success
-        then <ssh-config-file>
-        else builtins.trace ''
-          Please set your nix-path such that ssh-config-file points to a file that will allow ssh to access private repositories. The builder will not be able to see any running ssh agent sessions unless ssh-auth-sock is also set in the nix-path.
-
-          Note that the config file and any keys it points to must be readable by the build user, which depending on your nix configuration means making it readable by the build-users-group, the user of the running nix-daemon, or the user calling the nix command which started the build. Similarly, if using an ssh agent ssh-auth-sock must point to a socket the build user can access.
-
-          You may need StrictHostKeyChecking=no in the config file. Since ssh will refuse to use a group-readable private key, if using build-users you will likely want to use something like IdentityFile /some/directory/%u/key and have a directory for each build user accessible to that user.
-        '' "/var/lib/empty/config";
-    in builtins.toString sshConfigFile} "$@"
+    conf=$(mktemp)
+    echo "UserKnownHostsFile = $known_hostsPath" > $conf
+    exec -a ssh ${openssh}/bin/ssh -F $conf "$@"
   '';
 })
